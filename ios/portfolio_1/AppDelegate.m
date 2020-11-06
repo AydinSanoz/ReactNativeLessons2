b29b8395dae0989d5a1a7a914852e0ff3a9a5223
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-06 12:27:51 +0300

    api key cleaned from github

--- ios/portfolio_1/AppDelegate.m
+++ ios/portfolio_1/AppDelegate.m
@@ -27,7 +27,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyA8D8bc6V-GyGNbl43lY04WUi0fNKqPbp0"];
+  [GMSServices provideAPIKey:@" Your_ Api_Key "];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
